69c2eb22402e0e2b0804638f2b4acc5bcd37dd61
Gonzalo Garrido <gonzalo.garridoc@gmail.com>
2025-02-17 19:56:55 -0300

    Datos

--- analisis_ventas.py
+++ analisis_ventas.py
@@ -2,13 +2,14 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 # carga los datos desde el archivo csv
-df=pd.read_csv('ventas_productos.csv')
+df = pd.read_csv('ventas_productos.csv')
 
 #Calcual el precio total por producto
 
 df['Precio_Total']= df['Çantidad']* df['Precio' ]
                                        
 print(df.head())
+
 #Crea Grafic0 barra
 plt.bar(df['Producto'],df['Precio Total'])
 plt.xlabel('Prodcuto')
